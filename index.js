eea73149f2cc13221413cb84912ac94c24b79861
mukandalee <leonardmukanda254@gmail.com>
2022-12-04 15:33:29 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+const names = ["Guadalupe", "Ollie", "Aki"];
+event = 'suprise'
+
+
+function writeCards(names, event){
+  let appreciationCards=[];
+for (let i=0;  i< names.length; i++){
+  appreciationCards.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+  }
+  return appreciationCards;
+}
+function countDown(startInt){
+    while (startInt > 0) {
+      console.log(startInt);
+      startInt -= 1;
+    }
+    console.log(startInt);
+}
